4a486ecc04114ae7ebf0ba739e6c03e1f4959403
Bounif Lynda <l_bounif@esi.dz>
2020-05-11 15:33:06 +0100

    add event handler method to UserInput

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
 import UserOutput from './UserOutput/UserOutput';
+import UserInput from './UserInput/UserInput';
 
 class App extends Component {
+  state = {
+    username: "Lynda"
+  }
+  usernameChangedHandler = (event) => {
+    this.setState({
+      username:event.target.value
+    })
+  }
   render() {
     return (
       <div className="App">
-          <UserOutput username = "Lynda"> </UserOutput>
+          <UserInput changed = {this.usernameChangedHandler}> </UserInput>
+          <UserOutput username = {this.state.username}> </UserOutput>
           <UserOutput username = "Damia"> </UserOutput>
           <UserOutput username = "Ania"> </UserOutput>
       </div>
