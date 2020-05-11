79df0a30ce3f3edd77405c20dda8abb764d4dd67
Bounif Lynda <l_bounif@esi.dz>
2020-05-11 14:38:03 +0100

    Init project with two components

---
+++ src/UserOutput/UserOutput.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const userOutput = (props) => {
+    return (
+        <div>
+            <p>Hello this is Paragraph one</p>
+            <p>Hello this is Paragraph two</p>
+        </div>
+      )
+    }
+    
+export default userOutput
